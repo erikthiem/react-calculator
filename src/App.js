a8a17d3aef7ae405ef9c8971c1f374ab8ea398a4
Erik Thiem <erikthiem@gmail.com>
2021-05-01 19:11:52 -0400

    Start skeleton of custom calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+// Emulates a TI-108 Solar Calculator
 import './App.css';
 
-function App() {
+function Button(props) {
+  return <button className="button">{props.label}</button>;
+}
+
+function Screen(props) {
+  return <div className="screen">{props.value}</div>
+}
+
+function SolarPanel(props) {
+  return <div className="solarpanel"></div>
+}
+
+function Logo(props) {
+  return <div className="logo">TEXAS INSTRUMENTS TI-108</div>
+}
+
+function Calculator() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <Screen value="0."/>
+      <Logo/>
+      <SolarPanel/>
+      <Button label={"+/-"}/>
+      <Button label={"√"}/>
+      <Button label={"%"}/>
+      <Button label={"+"}/>
+      <br/>
+      <Button label={"MRC"}/>
+      <Button label={"M-"}/>
+      <Button label={"M+"}/>
+      <Button label={"×"}/>
+      <br/>
+      <Button label={7}/>
+      <Button label={8}/>
+      <Button label={9}/>
+      <Button label={"-"}/>
+      <br/>
+      <Button label={4}/>
+      <Button label={5}/>
+      <Button label={6}/>
+      <Button label={"+"}/>
+      <br/>
+      <Button label={1}/>
+      <Button label={2}/>
+      <Button label={3}/>
+      <Button label={"="}/>
+      <br/>
+      <Button label={"ON/C"}/>
+      <Button label={0}/>
+      <Button label={"."}/>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
